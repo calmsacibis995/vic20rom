336e3e62eb1384290de35e8e16c60649b3e3ad00
calmsacibis995 <46319359+calmsacibis995@users.noreply.github.com>
2022-09-19 21:35:42 +0300

    Add files via upload

---
+++ basic/basic.s
@@ -0,0 +1,79 @@
+;****************************************
+;*                                      *
+;* BBBBB   AAAA   SSS  IIII  CCCC       *
+;* BB  BB AA  AA SS  S  II  CC   C      *
+;* BB  BB AA  AA  SS    II  CC          *
+;* BBBBB  AAAAAA   SS   II  CC          *
+;* BB  BB AA  AA    SS  II  CC          *
+;* BB  BB AA  AA S  SS  II  CC   C      *
+;* BBBBB  AA  AA  SSS  IIII  CCCC       *
+;*                                      *
+;****************************************
+;
+;****************************************
+;*                                      *
+;* PET BASIC VERSION 2.1                *
+;*      THIS VERSION OF PET BASIC IS    *
+;* DESIGNED TO FUNCTION WITH THE KERNAL *
+;* PROVIDED IN THE FOLOWING MACHINES:   *
+;*      2001-4/8 (B/N)                  *
+;*      4016/4032                       *
+;*      8032                            *
+;*      TOY                             *
+;*      VIC-1001/-20                    *
+;*                                      *
+;* COPYRIGHT (C) 1981 BY                *
+;* COMMODORE BUSINESS MACHINES          *
+;*                                      *
+;****************************************
+;****LISTING DATE --1200 06 MARCH 1980***
+;****************************************
+;* THIS SOFTWARE IS FURNISHED FOR USE IN*
+;* THE ABOVE LISTED MACHINES ONLY.      *
+;*                                      *
+;* COPIES THEREOF MAY NOT BE PROVIDED   *
+;* OR MADE AVAILABLE FOR USE ON ANY     *
+;* SYSTEM.                              *
+;*                                      *
+;* THE INFORMATION IN THIS DOCUMENT IS  *
+;* SUBJECT TO CHANGE WITHOUT NOTICE.    *
+;*                                      *
+;* NO RESPONSIBILITY IS ASSUMED FOR     *
+;* RELIABILITY OF THIS SOFTWARE.        *
+;*                                      *
+;****************************************
+.feature labels_without_colons, pc_assignment
+
+.include "declare.s"
+.include "tokens.s"
+.include "token2.s"
+.include "code1.s"
+.include "code2.s"
+.include "code3.s"
+.include "code4.s"
+.include "code5.s"
+.include "code6.s"
+.include "code7.s"
+.include "code8.s"
+.include "code9.s"
+.include "code10.s"
+.include "code11.s"
+.include "code12.s"
+.include "code13.s"
+.include "code14.s"
+.include "code15.s"
+.include "code16.s"
+.include "code17.s"
+.include "code18.s"
+.include "code19.s"
+.include "code20.s"
+.include "code21.s"
+.include "code22.s"
+.include "code23.s"
+.include "code24.s"
+.include "code25.s"
+.include "code26.s"
+.include "trig.s"
+.include "init.s"
+
+;2022-09-19: Updated basic.s for use with cc65.
